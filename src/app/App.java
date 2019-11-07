a5f187164f7c0bb725bcc26c935c1285e6f821fc
leuaxe <leuaxe-488@hotmail.com>
2019-11-06 23:26:03 -0500

    prueba de imprimeListadoAlumnos exitoso

--- src/app/App.java
+++ src/app/App.java
@@ -3,5 +3,12 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
         Alumno alu = new Alumno("Juan", "Perez", "23199873", "Tercero", 9.0);
+
+        Alumno[] almacenaAlumnos = new Alumno[1];
+        almacenaAlumnos[0] = alu;
+        Profesor p1 = null;
+
+        Aula aula1 = new Aula(almacenaAlumnos, p1, "101");
+        aula1.imprimeListadoAlumnos();
     }
 }
